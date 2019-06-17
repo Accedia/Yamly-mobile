40ebb48b670c16f5fc049bae4a214e7ec6fbe3b3
Georgi Buyukliev <georgi.buyukliev@accedia.com>
2019-06-17 03:31:33 +0300

    Added the special product and removed some unused assets

--- yamly/lib/services/api_service.dart
+++ yamly/lib/services/api_service.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:yamly/models/recipe_model.dart';
 import 'package:yamly/values/urls.dart';
 
-class APIService{
+class APIService {
   static const int productsFetchNumber = 20;
   dynamic _extractData(http.Response resp) => json.decode(resp.body);
 
@@ -14,22 +14,23 @@ class APIService{
     return Exception('Server error; cause: $e');
   }
 
-  Future<void> addProduct() async{
-    if (data.products.length < 2 * productsFetchNumber)
-    {
+  Future<void> addProduct() async {
+    if (data.products.length < 2 * productsFetchNumber) {
       var newProducts = await fetchProducts();
-      if (newProducts != null){
+      if (newProducts != null) {
         data.products.addAll(newProducts);
+        addHotDog();
       }
     }
   }
 
   Future<List<ProductModel>> fetchProducts() async {
-    try{
+    try {
       final response = await http.get(Urls.randomProducts);
       if (response.statusCode == 200) {
         var productList = (_extractData(response) as List)
-              .map((value) => ProductModel.fromJson(value)).toList();
+            .map((value) => ProductModel.fromJson(value))
+            .toList();
         return productList;
       } else {
         return null;
@@ -40,13 +41,24 @@ class APIService{
   }
 
   Future fetchRecipe() async {
-     try {
-       final response = await http.get('${Urls.randomRecipe}/${data.user.uid}');
-       if (response.statusCode == 200) {
-         data.recipe = RecipeModel.fromJson(_extractData(response));
-       }
-     } catch(e) {
-       throw _handleError(e);
-     }
+    try {
+      final response = await http.get('${Urls.randomRecipe}/${data.user.uid}');
+      if (response.statusCode == 200) {
+        data.recipe = RecipeModel.fromJson(_extractData(response));
+      }
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  addHotDog() {
+    data.products.insert(
+        10,
+        ProductModel(
+            id: 0,
+            name: 'hot dog',
+            description: '',
+            imageUrl:
+                'https://veggiedesserts.co.uk/wp-content/uploads/2018/04/Lentil-Carrot-Hot-Dogs-4B.jpg'));
   }
 }
